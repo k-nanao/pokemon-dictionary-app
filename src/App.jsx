17d513cabcaae8633470e2896ee734da424d0a5f
zyyx-k-nanao <k-nanao@zyyx.jp>
2022-10-17 08:34:44 +0900

    ポケモンのデータ取得に成功

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  //非同期処理で読み込んでいるときのローディングを格納する。最初のtrueはuseEffectで初期読み込みでも再現させるため
+  const [loading, setLoading] = useState(true);
+  //_pokemonDataを格納する
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+      //各ポケモンの書斎なデータを取得、中にアクセスしてfetchで再び取得する
+      loadPokemon(res.results);
+
+      setLoading(false);
     };
+    fetchPokemonData();
   }, []);
 
-  return <div className='App'></div>;
+  //loadPokemonの引数をdataで受け取る
+  const loadPokemon = async (data) => {
+    //Promise.allは今回のfetchで呼び出すデータは20個あった。その20個全て呼び出すまでallにはその意味がある。map関数で展開できたのはPromise.allで呼び出しものは配列になる
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <div className='App'>
+      {loading ? (
+        <h1>ロード中</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました</h1>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/utils/pokemon.jsx
+++ src/utils/pokemon.jsx
@@ -1,7 +1,15 @@
 export const getAllPokemon = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
-      .then((res) => res.jaon())
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
       .then((data) => resolve(data));
   });
 };
